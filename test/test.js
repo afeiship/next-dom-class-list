4359eb5737c33dbae580eb31b57ebb7c2a6ff93e
afeiship <1290657123@qq.com>
2019-02-28 10:15:45 +0800

    add: test app.html

--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
-(function () {
+(function() {
   var nx = require('next-js-core2');
   var NxDomClassList = require('../src/next-dom-class-list');
+  var html = require('fs').readFileSync('./test/app.html').toString();
 
-  describe('NxDomClassList.methods', function () {
-    test('init', function () {
-      var data = {
-        key: 1,
-        value: 2
-      };
-      // console.log(rs1, rs2);
 
-      // expect(rs1).toBe(true);
+  document.documentElement.innerHTML = html;
+  var btnAdd = document.getElementById('add');
+  var removeAdd = document.getElementById('remove');
+  var toggleBtn = document.getElementById('toggle');
+  var containsBtn = document.getElementById('contains');
 
+
+  describe('NxDomClassList.methods', function() {
+    test('init', function() {
+      console.log(
+        btnAdd
+      );
     });
   });
-}());
+})();
